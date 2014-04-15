53410ea2438d8a9a5e328e02f898f30c57605846
Ruben Epong <rubenjohne@gmail.com>
2014-04-15 00:13:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+V2x::Application.config.secret_key_base = 'f467ca401bf6e51e27d23c5dc90fcd45adc1b2143162f8a34ac6d26566362395e55c9f76eb5251a416186dab1422e69ff5b6c45a8ce37dd173a95c5d5824a871'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+V2x::Application.config.session_store :cookie_store, key: '_v2x_session'
